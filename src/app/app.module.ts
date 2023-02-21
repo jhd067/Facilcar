ce21fa6c0a4fbffc89120f1310b1f65e8cce4154
jhd067 <jhd067@hotmail.com>
2023-02-20 23:34:46 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FinanciaComponent } from './components/financia/financia.component';
 import { PlanesComponent } from './components/planes/planes.component';
 import { ContactenosComponent } from './components/contactenos/contactenos.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 
